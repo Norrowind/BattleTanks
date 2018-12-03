629436e48e70e73e9e5041f41a5d13a61b03664e
Norrowind <igoranoshin8@gmail.com>
2018-12-03 16:09:59 +0200

    0.0.8 - Open lvl BattleGround when push start(also made Main Menu controller friendly). Create Tank.h and TankPlayerController.h(made them parent classes for BP analogs). UE_LOG cheching for TankPlayerController.h possesed to pawn. TankAIController.h attched to all pawns apper in world. UE_LOG chech, that AIController of each Tank can find player's tank in the world

---
+++ BattleTanks/Source/BattleTanks/Private/TankAIController.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/TankAIController.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController cant find player tank!"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *(PlayerTank->GetName()))
+	}
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn)
+	{
+		return nullptr;
+	}
+	else
+	{
+		return Cast<ATank>(PlayerPawn);
+	}
+}
+
